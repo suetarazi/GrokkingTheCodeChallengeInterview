ef0bdc81b6186275e37cebe1e3cdbff6dd44a6fd
Sue Tarazi <suemachtley@gmail.com>
2020-10-17 17:55:05 -0700

    all tests written

--- SmallestSubarrayWithAGivenSum/XUnitTestProject1/UnitTest1.cs
+++ SmallestSubarrayWithAGivenSum/XUnitTestProject1/UnitTest1.cs
@@ -9,7 +9,32 @@ namespace XUnitTestProject1
         [Fact]
         public void Test1()
         {
+            int[] TestInput = new int[] { 2, 1, 5, 2, 3, 2 };
+            int testS = 7;
+            Program p = new Program();
+            int actual = p.findMinSubArray(testS, TestInput);
+            int expected = 2;
+            Assert.Equal(expected, actual);
+        }
 
+        public void Test2()
+        {
+            int[] TestInput = new int[] { 2, 1, 5, 2, 8 };
+            int testS = 7;
+            Program p = new Program();
+            int actual = p.findMinSubArray(testS, TestInput);
+            int expected = 1;
+            Assert.Equal(expected, actual);
+        }
+
+        public void Test3()
+        {
+            int[] TestInput = new int[] { 3, 4, 1, 1, 6 };
+            int testS = 8;
+            Program p = new Program();
+            int actual = p.findMinSubArray(testS, TestInput);
+            int expected = 3;
+            Assert.Equal(expected, actual);
         }
     }
 }
