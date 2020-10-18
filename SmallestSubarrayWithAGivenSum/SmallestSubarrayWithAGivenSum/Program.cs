401095a6ade2fd647e18baa069168f2099dcc140
Sue Tarazi <suemachtley@gmail.com>
2020-10-17 17:35:14 -0700

    initial scaffold

---
+++ SmallestSubarrayWithAGivenSum/SmallestSubarrayWithAGivenSum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmallestSubarrayWithAGivenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int findMinSubArray(int S, int[] arr)
+        {
+            // TODO: Write your code here
+            return -1;
+        }
+    }
+}
