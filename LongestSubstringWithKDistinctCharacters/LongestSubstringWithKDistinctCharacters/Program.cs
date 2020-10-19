06a2142812c057a6e3ba0e2fd34ca798d8523791
Sue Tarazi <suemachtley@gmail.com>
2020-10-19 12:29:53 -0700

    all tests written for TDD

--- LongestSubstringWithKDistinctCharacters/LongestSubstringWithKDistinctCharacters/Program.cs
+++ LongestSubstringWithKDistinctCharacters/LongestSubstringWithKDistinctCharacters/Program.cs
@@ -2,14 +2,14 @@
 
 namespace LongestSubstringWithKDistinctCharacters
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
 
-        public static int findLength(String str, int k)
+        public int findLength(String str, int k)
         {
             // TODO: Write your code here
             return -1;
