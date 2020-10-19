59f2bbd706ec945d9f993e03046045a1494dd9c3
Sue Tarazi <suemachtley@gmail.com>
2020-10-19 12:23:50 -0700

    initial scaffold

---
+++ LongestSubstringWithKDistinctCharacters/LongestSubstringWithKDistinctCharacters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LongestSubstringWithKDistinctCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int findLength(String str, int k)
+        {
+            // TODO: Write your code here
+            return -1;
+        }
+
+    }
+}
