73ce3281ba91e688e1b48af858bb7e93d4c87efa
Sue Tarazi <suemachtley@gmail.com>
2020-10-19 13:51:49 -0700

    trying to translate Java HashMap to C# Dictionary

--- LongestSubstringWithKDistinctCharacters/LongestSubstringWithKDistinctCharacters/Program.cs
+++ LongestSubstringWithKDistinctCharacters/LongestSubstringWithKDistinctCharacters/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.Design;
 
 namespace LongestSubstringWithKDistinctCharacters
@@ -21,15 +22,33 @@ namespace LongestSubstringWithKDistinctCharacters
 
             int windowStart = 0;
             int maxSubstring = Int32.MinValue;
-            HashSet<char> hashmap = new HashSet<char>();
-            
+            Dictionary<char, int> hashmap = new Dictionary<char, int>();
+
+            //in this loop, we try and extend the width of the window [windowStart, windowEnd]            
             for(int windowEnd = 0; windowEnd < str.Length; windowEnd++)
             {
-                maxSubstring += str[windowEnd];
-                hashmap.Add(maxSubstring);
-                int linqQuery = Select(maxSubstring).Distinct
+
+                char rightChar = str[windowEnd];
+
+                hashmap.Add(rightChar, hashmap.TryGetValue(rightChar) + 1);
+                
+                while(hashmap.Count > k)
+                {
+                    char leftChar = str[windowStart];
+
+                    hashmap.Add(leftChar, hashmap[leftChar] -1;   
+                    
+                    if(hashmap.TryGetValue(leftChar) == 0)
+                    {
+                        hashmap.Remove(leftChar);
+                    }
+                    windowStart++;
+                }
+
+                maxSubstring = Math.Max(maxSubstring, windowEnd - windowStart + 1);
             }
-            return -1;
+
+            return maxSubstring;
         }
 
     }
