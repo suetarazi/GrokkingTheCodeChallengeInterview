98cac73f839ff52f7f0577b894e32509571f4f63
Sue Tarazi <suemachtley@gmail.com>
2020-10-21 16:22:18 -0700

    done :)

--- ShortestPathLength/ShortestPathLength/Program.cs
+++ ShortestPathLength/ShortestPathLength/Program.cs
@@ -12,19 +12,48 @@ namespace ShortestPathLength
 
         public static int shortestPathLength(Node start, Node end)
         {
+            //create a variable to count steps
+            int steps = 0;
+
+            //make a scratch pad to keep track of which nodes have been visited and their respective values
+            Dictionary<Node, int> scratch = new Dictionary<Node, int>();
+
+            //make a queue to employ breadth-first-traversal
+            Queue<Node> queue = new Queue<Node>();
+
             // Start at the start
+            queue.Enqueue(start);
+            scratch.Add(start, start.id);
+            
             // Create a set of nodes S that are reachable from start
+            while(queue.Count != 0)
+            {
+                Node n = queue.Dequeue();
+
+                foreach(Node nei in n.neighbors)
+                {
+                    if(!scratch.ContainsKey(nei))
+                    {
+                        queue.Enqueue(nei);
+                        scratch.Add(nei, nei.id);
+
+                    }
+                }
+
+                steps++;
+
+                foreach(Node nei in n.neighbors)
+                {
+                    if(nei.id == end.id)
+                    {
+                        return steps;
+                    }
+                }
+            }
+            
             // Create a set of nodes S' thar are reachable from S and have not yet been in any set
             // Repeat until you reach end
             // Return number of sets (+/-1?)
-
-
-
-            Queue<Node> queue = new Queue<Node>();
-            queue.Enqueue(start); //{0,0}
-
-
-
 
             return -1;
 
