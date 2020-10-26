f04d15977207e8a5e72b695d994c7b8116f787ef
Sue Tarazi <suemachtley@gmail.com>
2020-10-26 10:00:25 -0700

    initial code

--- NoRepeatSubstring/NoRepeatSubstring/Program.cs
+++ NoRepeatSubstring/NoRepeatSubstring/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NoRepeatSubstring
 {
@@ -12,8 +13,40 @@ namespace NoRepeatSubstring
         //find the length of the longest substring which has no repeating characters
         public int findLength(string str)
         {
+            int windowStart = 0;
+            int maxSubstring = 1;
 
-            return -1;
+            List<char> charList = new List<char>();
+
+            //grow the window
+            for(int windowEnd = 0; windowEnd < str.Length; windowEnd++)
+            {
+                char rightChar = str[windowEnd];
+
+                if(charList.Contains(rightChar))
+                {
+                    charList.Remove(rightChar);
+                }                
+                charList.Add(rightChar);
+                
+                while(charList.Count > 0)
+                {
+                    char leftChar = str[windowStart];
+
+                    if(charList.Contains(leftChar))
+                    {
+                        charList.Remove(leftChar);
+                    }
+
+                    windowStart++;
+                }
+
+                maxSubstring = Math.Max(maxSubstring, windowEnd - windowStart + 1);
+            }
+
+
+            return maxSubstring;
         }
+
     }
 }
