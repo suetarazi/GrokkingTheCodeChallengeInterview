92ddff0812bacc26450bd974dc867bb06095ae2e
Sue Tarazi <suemachtley@gmail.com>
2020-10-26 19:11:23 -0700

    code done

--- LongestSubstringWithSameLettersAfterReplacement/LongestSubstringWithSameLettersAfterReplacement/Program.cs
+++ LongestSubstringWithSameLettersAfterReplacement/LongestSubstringWithSameLettersAfterReplacement/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LongestSubstringWithSameLettersAfterReplacement
 {
@@ -13,16 +14,38 @@ namespace LongestSubstringWithSameLettersAfterReplacement
         {
             int windowStart = 0;
             int maxSubstring = 0;
+            int maxRepeatLetterCount = 0;
             Dictionary<char, int> map = new Dictionary<char, int>();
 
             for(int windowEnd = 0; windowEnd < str.Length; windowEnd++)
             {
+                rightChar = str[windowEnd];
+                //first, put all the characters in the map
+                if(!map.Contains(rightChar))
+                {
+                    map.Add(rightChar, 1);
+                }
+                else
+                {
+                    map[rightChar] += 1;
+                }
 
+                //get the character with the largest value from the map
+                var maxRepeatLetterCount = Math.Max(maxRepeatLetterCount, map.TryGetValue(windowEnd));
+
+                //current window size is from windowStart to windowEnd, so overall we have a letter that is repeating 'maxRepeatLetterCount' times. So, we therefore have a window with maxRepeatLetterCount times of one letter and the remaining letters we should try and replace. If the remaining letters are > k, then we need to shrink our window since we can only replace no more than k letters. 
+                if(windowEnd - windowStart + 1 - maxRepeatLetterCount > k)
+                {
+                    char leftChar = str[windowStart];
+
+                    map.Add[leftChar] - 1;
+
+                    windowStart++;
+                }
+
+                maxSubstring = Math.Max(maxSubstring, windowStart - windowEnd + 1);
 
             }
-
-            maxSubstring = Math.Max(maxSubstring, windowStart - windowEnd + 1);
-
 
             return maxSubstring;
         }
