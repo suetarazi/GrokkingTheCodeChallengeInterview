d478bc20d4bec44f58d96c850acb52270c2b86d1
Sue Tarazi <suemachtley@gmail.com>
2020-10-26 18:44:57 -0700

    initial code

---
+++ LongestSubstringWithSameLettersAfterReplacement/LongestSubstringWithSameLettersAfterReplacement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LongestSubstringWithSameLettersAfterReplacement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int findLength(String str, int k)
+        {
+            int windowStart = 0;
+            int maxSubstring = 0;
+            Dictionary<char, int> map = new Dictionary<char, int>();
+
+            for(int windowEnd = 0; windowEnd < str.Length; windowEnd++)
+            {
+
+
+            }
+
+            maxSubstring = Math.Max(maxSubstring, windowStart - windowEnd + 1);
+
+
+            return maxSubstring;
+        }
+    }
+}
